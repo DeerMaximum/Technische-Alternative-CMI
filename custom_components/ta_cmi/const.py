4df3b00418518f1def11119797bf6f03fbc7c59c
Maximilian <43999966+DeerMaximum@users.noreply.github.com>
2023-05-06 13:21:40 +0000

    Extend default device_class mapping (#54)

--- custom_components/ta_cmi/const.py
+++ custom_components/ta_cmi/const.py
@@ -4,8 +4,8 @@ from __future__ import annotations
 from datetime import timedelta
 from logging import Logger, getLogger
 
-from ta_cmi import ChannelType
 from homeassistant.components.sensor import SensorDeviceClass
+from ta_cmi import ChannelType
 
 _LOGGER: Logger = getLogger(__package__)
 
@@ -34,13 +34,28 @@ DEFAULT_DEVICE_CLASS_MAP: dict[str, SensorDeviceClass] = {
     "°C": SensorDeviceClass.TEMPERATURE,
     "K": SensorDeviceClass.TEMPERATURE,
     "A": SensorDeviceClass.CURRENT,
+    "mA": SensorDeviceClass.CURRENT,
     "kWh": SensorDeviceClass.ENERGY,
-    "m³": SensorDeviceClass.GAS,
+    "MWh": SensorDeviceClass.ENERGY,
+    "km/h": SensorDeviceClass.SPEED,
+    "m/s": SensorDeviceClass.SPEED,
+    "Hz": SensorDeviceClass.FREQUENCY,
+    "km": SensorDeviceClass.DISTANCE,
+    "m": SensorDeviceClass.DISTANCE,
+    "mm": SensorDeviceClass.DISTANCE,
+    "cm": SensorDeviceClass.DISTANCE,
     "%": SensorDeviceClass.HUMIDITY,
+    "kg": SensorDeviceClass.WEIGHT,
+    "t": SensorDeviceClass.WEIGHT,
+    "g": SensorDeviceClass.WEIGHT,
     "lx": SensorDeviceClass.ILLUMINANCE,
     "W": SensorDeviceClass.POWER,
+    "kW": SensorDeviceClass.POWER,
     "mbar": SensorDeviceClass.PRESSURE,
+    "bar": SensorDeviceClass.PRESSURE,
+    "Pa": SensorDeviceClass.PRESSURE,
     "V": SensorDeviceClass.VOLTAGE,
+    "W/m²": SensorDeviceClass.IRRADIANCE,
 }
 
 TYPE_BINARY = "binary"
